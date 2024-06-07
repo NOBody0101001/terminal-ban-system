2c0c64fac28dd817cbcf2a1d09dcd700d13d94d4
protocolhere <vodka06770@gmail.com>
2024-06-07 20:41:57 +0300

    Update terminalban.py

--- terminalban.py
+++ terminalban.py
@@ -1,76 +1,147 @@
 import os
+import shutil
 
-# Yasaklı kullanıcıların listesini dosyada sakla
-ban_file = "ban_list.txt"
+home_dir = os.path.expanduser("~")
+ban_file_path = os.path.join(home_dir, "ban_list.txt")
 
-# Yasaklı kullanıcı listesini yükleyen fonksiyon
+ban_list_memory = {}
+
 def load_ban_list():
-    if not os.path.exists(ban_file):
-        return {}
-    with open(ban_file, "r") as file:
+    global ban_list_memory
+    if not os.path.exists(ban_file_path):
+        ban_list_memory = {}
+        return
+    with open(ban_file_path, "r") as file:
         lines = file.readlines()
-    ban_list = {}
+    ban_list_memory = {}
     for line in lines:
-        user_id, user_name = line.strip().split(",")
-        ban_list[user_id] = user_name
-    return ban_list
+        user_id, user_name, user_surname = line.strip().split(",")
+        ban_list_memory[user_id] = (user_name, user_surname)
 
-# Yasaklı kullanıcı listesini kaydeden fonksiyon
-def save_ban_list(ban_list):
-    with open(ban_file, "w") as file:
-        for user_id, user_name in ban_list.items():
-            file.write(f"{user_id},{user_name}\n")
+def save_ban_list():
+    global ban_list_memory
+    with open(ban_file_path, "w") as file:
+        for user_id, (user_name, user_surname) in ban_list_memory.items():
+            file.write(f"{user_id},{user_name},{user_surname}\n")
 
-# Kullanıcıyı yasaklayan fonksiyon
-def ban_user(user_id, user_name):
-    ban_list = load_ban_list()
-    ban_list[user_id] = user_name
-    save_ban_list(ban_list)
-    print(f"Kullanıcı {user_name} (ID: {user_id}) yasaklandı.")
+def ban_user(user_id, user_name, user_surname):
+    global ban_list_memory
+    ban_list_memory[user_id] = (user_name, user_surname)
+    save_ban_list()
+    print(f"Kullanıcı {user_name} {user_surname} (ID: {user_id}) yasaklandı.")
 
-# Kullanıcı adıyla yasaklı kontrolü yapan fonksiyon
 def check_ban_by_name(user_name):
-    ban_list = load_ban_list()
-    for uid, uname in ban_list.items():
+    global ban_list_memory
+    for uid, (uname, usurname) in ban_list_memory.items():
         if uname == user_name:
             print("\033[91mYasaklandı\033[0m")
             return
     print("\033[92mYasaklı değil\033[0m")
 
-# Kullanıcı ID'siyle yasaklı kontrolü yapan fonksiyon
+def check_ban_by_surname(user_surname):
+    global ban_list_memory
+    for uid, (uname, usurname) in ban_list_memory.items():
+        if usurname == user_surname:
+            print("\033[91mYasaklandı\033[0m")
+            return
+    print("\033[92mYasaklı değil\033[0m")
+
 def check_ban_by_id(user_id):
-    ban_list = load_ban_list()
-    if user_id in ban_list:
+    global ban_list_memory
+    if user_id in ban_list_memory:
         print("\033[91mYasaklandı\033[0m")
     else:
         print("\033[92mYasaklı değil\033[0m")
 
-# Ana menü fonksiyonu
+def unban_user(user_id=None, user_name=None, user_surname=None):
+    global ban_list_memory
+    removed = False
+    if user_id and user_id in ban_list_memory:
+        del ban_list_memory[user_id]
+        removed = True
+    elif user_name:
+        for uid, (uname, usurname) in list(ban_list_memory.items()):
+            if uname == user_name:
+                del ban_list_memory[uid]
+                removed = True
+                break
+    elif user_surname:
+        for uid, (uname, usurname) in list(ban_list_memory.items()):
+            if usurname == user_surname:
+                del ban_list_memory[uid]
+                removed = True
+                break
+    if removed:
+        save_ban_list()
+        print("\033[92mYasak kaldırıldı\033[0m")
+    else:
+        print("\033[91mKullanıcı bulunamadı\033[0m")
+
+# Reklam mesajı fonksiyonu
+def display_advertisement():
+    text_art = """
+     ⠀⠀⠀⠀⠀⠀⠸⣶⣦⡄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣀⣀⣀⡀⢀⠀⢹⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣷⣄⠨⣿⣿⣿⡌⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣷⣿⣿⣿⣿⣿⣶⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⣴⣾⣿⣮⣝⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠈⠉⠙⠻⢿⣿⣿⣿⣿⣿⣿⠟⣹⣿⡿⢿⣿⣿⣬⣶⣶⡶⠦⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⣢⣙⣻⢿⣿⣿⣿⠎⢸⣿⠕⢹⣿⣿⡿⣛⣥⣀⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠉⠛⠿⡏⣿⡏⠿⢄⣜⣡⠞⠛⡽⣸⡿⣟⡋⠉⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠾⠿⣿⠁⠀⡄⠀⠀⠰⠾⠿⠛⠓⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠠⢐⢉⢷⣀⠛⠠⠐⠐⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣠⣴⣶⣿⣧⣾⠡⠼⠎⢎⣋⡄⠆⠀⠱⡄⢉⠃⣦⡤⡀⠀⠀⠀⠀
+⠀⠀⠐⠙⠻⢿⣿⣿⣿⣿⣿⣿⣄⡀⠀⢩⠀⢀⠠⠂⢀⡌⠀⣿⡇⠟⠀⠀⢄⠀
+⠀⣴⣇⠀⡇⠀⠸⣿⣿⣿⣿⣽⣟⣲⡤⠀⣀⣠⣴⡾⠟⠀⠀⠟⠀⠀⠀⠀⡰⡀
+⣼⣿⠋⢀⣇⢸⡄⢻⣟⠻⣿⣿⣿⣿⣿⣿⠿⡿⠟⢁⠀⠀⠀⠀⠀⢰⠀⣠⠀⠰
+⢸⣿⡣⣜⣿⣼⣿⣄⠻⡄⡀⠉⠛⠿⠿⠛⣉⡤⠖⣡⣶⠁⠀⠀⠀⣾⣶⣿⠐⡀
+⣾⡇⠈⠛⠛⠿⣿⣿⣦⠁⠘⢷⣶⣶⡶⠟⢋⣠⣾⡿⠃⠀⠀⠀⠰⠛⠉⠉⠀⠀ 
+    """
+    print(text_art)
+    print("\033[91m made by protocolhere 😈\033[0m")
+
 def main():
+    load_ban_list()
     while True:
-        print("\n1. Name Check\n2. ID Check\n3. Add Ban\n4. Exit")
+        display_advertisement()
+        print("\n1. Name Check\n2. Surname Check\n3. ID Check\n4. Add Ban\n5. Remove Ban\n6. Exit")
         choice = input("Bir seçenek seçin: ")
         
         if choice == "1":
-            user_name = input("Kullanıcı adını girin: ")
-            check_ban_by_name(user_name)
+            user_name = input("Kullanıcı adını girin (Opsiyonel): ")
+            if user_name:
+                check_ban_by_name(user_name)
+            else:
+                print("Kullanıcı adı girilmedi.")
         elif choice == "2":
-            user_id = input("Kullanıcı ID'sini girin: ")
-            check_ban_by_id(user_id)
+            user_surname = input("Kullanıcı soyadını girin (Opsiyonel): ")
+            if user_surname:
+                check_ban_by_surname(user_surname)
+            else:
+                print("Kullanıcı soyadı girilmedi.")
         elif choice == "3":
+            user_id = input("Kullanıcı ID'sini girin (Opsiyonel): ")
+            if user_id:
+                check_ban_by_id(user_id)
+            else:
+                print("Kullanıcı ID'si girilmedi.")
+        elif choice == "4":
             user_id = input("Yasaklamak istediğiniz kullanıcının ID'sini girin: ")
             user_name = input("Yasaklamak istediğiniz kullanıcının adını girin: ")
-            ban_user(user_id, user_name)
-        elif choice == "4":
+            user_surname = input("Yasaklamak istediğiniz kullanıcının soyadını girin: ")
+            ban_user(user_id, user_name, user_surname)
+        elif choice == "5":
+            user_id = input("Banını kaldırmak istediğiniz kullanıcının ID'sini girin (Opsiyonel): ")
+            user_name = input("Banını kaldırmak istediğiniz kullanıcının adını girin (Opsiyonel): ")
+            user_surname = input("Banını kaldırmak istediğiniz kullanıcının soyadını girin (Opsiyonel): ")
+            unban_user(user_id or None, user_name or None, user_surname or None)
+        elif choice == "6":
             break
         else:
             print("Geçersiz seçenek, lütfen tekrar deneyin.")
 
-# Program başlatma
 if __name__ == "__main__":
-    # ban_list.txt dosyasını oluşturma
-    if not os.path.exists(ban_file):
-        open(ban_file, 'w').close()
+    if not os.path.exists(ban_file_path):
+        open(ban_file_path, 'w').close()
     
-    # Ana fonksiyonu çalıştırma
     main()
